c5a54cf94b5ed34445951c66496d2e1278b3fde1
Frank Kair <frankkair@gmail.com>
2019-03-11 14:21:38 +0000

    Small coding style refactor following Nim's docs
    More information: https://nim-lang.org/docs/nep1.html

--- nemo.nim
+++ nemo.nim
@@ -2,12 +2,12 @@ import xmltree, nimquery, nimpy, sequtils, strtabs, tables
 from htmlparser import parseHtml
 from streams import newStringStream
 
-proc parse(html:string):XmlNode = newStringStream(html).parseHtml()
+proc parse(html: string): XmlNode = newStringStream(html).parseHtml()
 
-func fields(node:XmlNode):Table[string,string] = 
-  result = toTable({"text" : innerText(node), "tag" : tag(node)})
-  if attrsLen(node) > 0 : (for prop, val in attrs(node).pairs(): result[prop]=val)
+func fields(node: XmlNode): Table[string, string] = 
+  result = toTable({"text": innerText(node), "tag": tag(node)})
+  if attrsLen(node) > 0: (for prop, val in attrs(node).pairs(): result[prop]=val)
 
-proc find(html : string, selectors : seq[string]) : seq[(string, seq[Table[string, string]])] {.exportpy.} = 
+proc find(html: string, selectors: seq[string]): seq[(string, seq[Table[string, string]])] {.exportpy.} = 
   let parsed = parse(html)
   return selectors.mapIt((it, parsed.querySelectorAll(it).map(fields)))
